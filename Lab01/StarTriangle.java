0d5e3cc6f9cf6974fdf562a946dc1d679393cdab
Jesse <123838537+iJxsse@users.noreply.github.com>
2024-02-16 08:38:23 +0000

    Create StarTriangle.java

---
+++ Lab01/StarTriangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class StarTriangle {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int rows = scan.nextInt();
+        printStarTriangle(rows);
+    }
+
+    public static void printStarTriangle(int rows) {
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
